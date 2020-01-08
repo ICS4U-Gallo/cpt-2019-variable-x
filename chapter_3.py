d3bb09b6664c44c41af8f72b1b59a9ba883039aa
Al3xanderLee <34520973+Al3xanderLee@users.noreply.github.com>
2020-01-08 11:10:17 -0500

    Test

--- chapter_3.py
+++ chapter_3.py
@@ -1,34 +1,122 @@
+"""
+Example Game
+"""
+
 import arcade
 
-import settings
+# Constants
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 650
+SCREEN_TITLE = "Platformer Game"
 
+# Constants used to scale our sprites from their original size
+CHARACTER_SCALING = 1
+TILE_SCALING = 0.5
+COIN_SCALING = 0.5
 
-class Chapter3View(arcade.View):
-    def on_show(self):
-        arcade.set_background_color(arcade.color.BLUE_SAPPHIRE)
-    
+PLAYER_MOVEMENT_SPEED = 5
+GRAVITY = 0.1
+PLAYER_JUMP_SPEED = 200
+
+class MyGame(arcade.Window):
+    """
+    Main application class.
+    """
+    def __init__(self):
+        # Call the parent class and set up the window
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+
+        # These are 'lists' that keep track of our sprites. Each sprite should go into a list.
+        self.coin_list = None
+        self.wall_list = None
+        self.player_list = None
+
+        # Separate variable that holds the player sprite
+        self.player_sprite = None
+
+        arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
+
+    def setup(self):
+        """Set up the game here. Call this function to restart the game"""
+        # Create the Sprite lists
+        self.player_list = arcade.SpriteList()
+        self.wall_list = arcade.SpriteList()
+        self.coin_list = arcade.SpriteList()
+
+        # Set up the player, specifically placing it at these coordinates.
+        image_source = "images/e.png" 
+        self.player_sprite = arcade.Sprite(image_source, 0.5)
+        self.player_sprite.center_x = 64
+        self.player_sprite.center_y = 500
+        self.player_list.append(self.player_sprite)
+
+        # Create the ground
+        for x in range(0, 1250, 64):
+            wall = arcade.Sprite("images/grassMid.png", TILE_SCALING)
+            wall.center_x = x
+            wall.center_y = 32
+            self.wall_list.append(wall)
+
+        # Put some crates on the ground
+        # This shows using a coordinate list
+        coordinate_list = [[512, 96]]
+        
+        for coordinate in coordinate_list:
+            # Add a crate on the ground
+            wall = arcade.Sprite("images/boxCrate_double.png", TILE_SCALING)
+            wall.position = coordinate
+            self.wall_list.append(wall)
+        
+        self.physics_engine = arcade.PhysicsEnginePlatformer(self.player_sprite, self.wall_list, GRAVITY)
+
     def on_draw(self):
+        """Render the screen."""
+
+        # Clear the screen to the background color
         arcade.start_render()
-        arcade.draw_text("Chapter 3", settings.WIDTH/2, settings.HEIGHT/2,
-                         arcade.color.BLACK, font_size=30, anchor_x="center")
+        
+        # Code to draw the screen goes here
+        self.wall_list.draw()
+        self.coin_list.draw()
+        self.player_list.draw()
     
     def on_key_press(self, key, modifiers):
-        self.director.next_view()
+        """Called whenever a key is pressed. """
+
+        if key == arcade.key.UP or key == arcade.key.W:
+            self.player_sprite.change_y = PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+            self.player_sprite.change_y = -PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
+
+    def on_key_release(self, key, modifiers):
+        """Called when the user releases a key. """
+
+        if key == arcade.key.UP or key == arcade.key.W:
+            self.player_sprite.change_y = 0
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+            self.player_sprite.change_y = 0
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = 0
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = 0
+
+    def on_update(self, delta_time):
+        """ Movement and game logic """
+
+        # Move the player with the physics engine
+        self.physics_engine.update()
 
 
-if __name__ == "__main__":
-    """This section of code will allow you to run your View
-    independently from the main.py file and its Director.
-
-    You can ignore this whole section. Keep it at the bottom
-    of your code.
-
-    It is advised you do not modify it unless you really know
-    what you are doing.
-    """
-    from utils import FakeDirector
-    window = arcade.Window(settings.WIDTH, settings.HEIGHT)
-    my_view = Chapter3View()
-    my_view.director = FakeDirector(close_on_next_view=True)
-    window.show_view(my_view)
+def main():
+    """Main method"""
+    window = MyGame()
+    window.setup()
     arcade.run()
+
+
+if __name__== "__main__":
+    main()
