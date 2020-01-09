5db227f8b28e7977cc329e45de1cf39e83bc30bd
Al3xanderLee <34520973+Al3xanderLee@users.noreply.github.com>
2020-01-09 11:11:20 -0500

    Update chapter_3.py

--- chapter_3.py
+++ chapter_3.py
@@ -14,9 +14,17 @@ CHARACTER_SCALING = 1
 TILE_SCALING = 0.5
 COIN_SCALING = 0.5
 
-PLAYER_MOVEMENT_SPEED = 5
-GRAVITY = 0.1
+PLAYER_MOVEMENT_SPEED = 10
+GRAVITY = 1
 PLAYER_JUMP_SPEED = 200
+
+# How many pixels to keep as a minimum margin between the player and the edge 
+# of the screen. 
+LEFT_VIEWPORT_MARGIN = 400
+RIGHT_VIEWPORT_MARGIN = 400
+BOTTOM_VIEWPORT_MARGIN = 50
+TOP_VIEWPORT_MARGIN = 100
+
 
 class MyGame(arcade.Window):
     """
@@ -34,7 +42,10 @@ class MyGame(arcade.Window):
         # Separate variable that holds the player sprite
         self.player_sprite = None
 
-        arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
+        self.view_bottom = 0
+        self.view_left = 0 
+
+        arcade.set_background_color(arcade.csscolor.BLACK)
 
     def setup(self):
         """Set up the game here. Call this function to restart the game"""
@@ -44,15 +55,15 @@ class MyGame(arcade.Window):
         self.coin_list = arcade.SpriteList()
 
         # Set up the player, specifically placing it at these coordinates.
-        image_source = "images/e.png" 
-        self.player_sprite = arcade.Sprite(image_source, 0.5)
+        image_source = "images/boxCrate_double.png" 
+        self.player_sprite = arcade.Sprite(image_source, 0.2)
         self.player_sprite.center_x = 64
         self.player_sprite.center_y = 500
         self.player_list.append(self.player_sprite)
 
         # Create the ground
         for x in range(0, 1250, 64):
-            wall = arcade.Sprite("images/grassMid.png", TILE_SCALING)
+            wall = arcade.Sprite("images/grass_block.png", 0.2)
             wall.center_x = x
             wall.center_y = 32
             self.wall_list.append(wall)
@@ -110,6 +121,39 @@ class MyGame(arcade.Window):
         # Move the player with the physics engine
         self.physics_engine.update()
 
+        # ---Manage Scrolling ---
+
+        # Track if we need to change the viewport
+
+        changed = False
+
+        # Scroll left
+        left_boundary = self.view_left + LEFT_VIEWPORT_MARGIN
+        if self.player_sprite.left < left_boundary:
+            self.view_left -= left_boundary - self.player_sprite.left
+            changed = True
+        
+        # Scroll right
+        right_boundary = self.view_left + SCREEN_WIDTH - RIGHT_VIEWPORT_MARGIN
+        if self.player_sprite.right > right_boundary:
+            self.view_left += self.player_sprite.right - right_boundary
+            changed = True
+
+        top_boundary = self.view_bottom + SCREEN_HEIGHT - TOP_VIEWPORT_MARGIN
+        if self.player_sprite.bottom > top_boundary:
+            self.view_bottom += self.player_sprite.top - top_boundary
+            changed = True
+
+        bottom_boundary = self.view_bottom + BOTTOM_VIEWPORT_MARGIN
+        if self.player_sprite.bottom < bottom_boundary:
+            self.view_bottom -= self.player_sprite.bottom + bottom_boundary
+            changed = True
+         
+        if changed:
+            self.view_bottom = int(self.view_bottom)
+            self.view_left = int(self.view_left)
+
+        arcade.set_viewport(self.view_left, SCREEN_WIDTH + self.view_left, self.view_bottom, SCREEN_HEIGHT + self.view_bottom)
 
 def main():
     """Main method"""
@@ -120,3 +164,4 @@ def main():
 
 if __name__== "__main__":
     main()
+
