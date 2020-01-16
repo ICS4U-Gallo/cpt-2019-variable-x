a741793bf2d920c248089c1794505a9ed76df48d
LucasM212 <43610120+LucasM212@users.noreply.github.com>
2020-01-16 11:12:04 -0500

    Update zombieinvasion.py

--- zombieinvasion.py
+++ zombieinvasion.py
@@ -2,7 +2,7 @@ import random
 import arcade
 import os
 import json
-
+from typing import List
 
 SPRITE_SCALING_PLAYER = 0.5
 SPRITE_SCALING_ZOMBIE = 0.5
@@ -14,6 +14,46 @@ WIDTH = 800
 HEIGHT = 600
 SCREEN_TITLE = "Zombie Invasion"
 BULLET_SPEED = 5
+def merge_sort(numbers: List[int]) -> List[int]:
+    # base case
+    if len(numbers) == 1:
+        return numbers
+
+    midpoint = len(numbers)//2
+
+    # two recursive steps
+    # mergesort left
+    left_side = merge_sort(numbers[:midpoint])
+    # mergesort right
+    right_side = merge_sort(numbers[midpoint:])
+    # merge the two together
+    sorted_list = []
+
+    # loop through both lists with two markers
+    left_marker = 0
+    right_marker = 0
+    while left_marker < len(left_side) and right_marker < len(right_side):
+        # if right value less than left value, add right value to sorted, increase right marker
+        if left_side[left_marker] < right_side[right_marker]:
+            sorted_list.append(left_side[left_marker])
+            left_marker += 1
+        # if left value less than right value, add left value to sorted, increase left marker
+        else:
+            sorted_list.append(right_side[right_marker])
+            right_marker += 1
+    
+    # create a while loop to gather the rest of the values from either list
+    while right_marker < len(right_side):
+        sorted_list.append(right_side[right_marker])
+        right_marker += 1
+    
+    while left_marker < len(left_side):
+        sorted_list.append(left_side[left_marker])
+        left_marker += 1
+    
+    # return the sorted list
+    return sorted_list
+
 
 def bubblesort(numbers):
     n = len(numbers)
@@ -34,7 +74,7 @@ class MenuView(arcade.View):
         arcade.draw_text("Zombie Invasion", WIDTH/2, HEIGHT/2,
                          arcade.color.BLACK, font_size=50, anchor_x="center")
         arcade.draw_text("Click to go next", WIDTH/2, HEIGHT/2-75,
-                         arcade.color.GRAY, font_size=20, anchor_x="center")
+                         arcade.color.WHITE, font_size=20, anchor_x="center")
 
     def on_mouse_press(self, _x, _y, _button, _modifiers):
         instructions_view = InstructionView()
@@ -50,8 +90,7 @@ class InstructionView(arcade.View):
         arcade.draw_text("Instructions: \n Left arrow key to move left \n Right arrow key to move right \n Space to shoot \n Your mission is to eliminate all zombies that come your way. You have one life to do this. Good luck.", WIDTH/2, HEIGHT/2,
                          arcade.color.BLACK, font_size=12, anchor_x="center")
         arcade.draw_text("Click to begin", WIDTH/2, HEIGHT/2-75,
-                         arcade.color.GRAY, font_size=20, anchor_x="center")
-
+                         arcade.color.WHITE, font_size=20, anchor_x="center")
     def on_mouse_press(self, _x, _y, _button, _modifiers):
         game_view = MyGame()
         self.window.show_view(game_view)
@@ -67,7 +106,7 @@ class HighscoreInput(arcade.View):
         arcade.draw_text(f"The Zombies have made a breach in our defenses! \n You have fought valiently, achieving a zombie kill count of: {highscore}", WIDTH/2, HEIGHT/2,
                          arcade.color.BLACK, font_size=12, anchor_x="center")
         arcade.draw_text("Click to see other highscores", WIDTH/2, HEIGHT/2-75,
-                         arcade.color.GRAY, font_size=20, anchor_x="center")
+                         arcade.color.WHITE, font_size=20, anchor_x="center")
 
     def on_mouse_press(self, _x, _y, _button, _modifiers):
         highscore = MyGame().SCORE
@@ -87,6 +126,7 @@ class HighscoreView(arcade.View):
         arcade.set_background_color(arcade.color.AMAZON)
 
     def on_draw(self):
+        height_decrease = 120
         sorted_list = []
         arcade.start_render()
         with open("data.json", "r") as f:
@@ -95,10 +135,15 @@ class HighscoreView(arcade.View):
         for values in data.values():
             sorted_list.append(values)
             bubblesort(sorted_list)
-        arcade.draw_text(f"Highscores: \n {sorted_list}", WIDTH/2, HEIGHT/2,
+        arcade.draw_text(f"Highscores: \n", WIDTH/2, HEIGHT/2,
                          arcade.color.BLACK, font_size=18, anchor_x="center")
-        arcade.draw_text("Click to continue", WIDTH/2, HEIGHT/2-75,
-                         arcade.color.GRAY, font_size=20, anchor_x="center")
+        for i in reversed(range(5)):
+            arcade.draw_text(f"{i+1}: {sorted_list[i]} \n", WIDTH/2, HEIGHT/2-height_decrease,
+                             arcade.color.BLACK, font_size=18, anchor_x="center")
+            height_decrease -= 20
+        arcade.draw_text("Click to continue", WIDTH/2, HEIGHT/2-250,
+                         arcade.color.WHITE, font_size=20, anchor_x="center")
+
 
 
 class MyGame(arcade.View):
@@ -126,7 +171,6 @@ class MyGame(arcade.View):
             zombie.center_x = random.randrange(SCREEN_WIDTH)
             zombie.center_y = random.randrange(600, 800)
             zombie.change_y = -0.5
-
             self.zombie_list.append(zombie)
 
     def on_show(self):
@@ -196,7 +240,7 @@ class MyGame(arcade.View):
         if self.player_sprite.center_x > SCREEN_WIDTH - 20:
             self.player_sprite.center_x = SCREEN_WIDTH - 20
             
-        for zombie in self.zombie_list:
+        for zombie in self.zombie_list:       
             if zombie.center_y == 0:
                 self.lives -= 1
             if self.player_sprite.collides_with_sprite(zombie):
